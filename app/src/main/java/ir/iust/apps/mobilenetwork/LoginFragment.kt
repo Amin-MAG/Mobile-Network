ba5b8ccde6e4854d4a11f9ea3184622689bf42cc
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2022-04-23 03:47:51 +0430

    feat: add HW1 logics to the program

--- app/src/main/java/ir/iust/apps/mobilenetwork/LoginFragment.kt
+++ app/src/main/java/ir/iust/apps/mobilenetwork/LoginFragment.kt
@@ -1,14 +1,19 @@
 package ir.iust.apps.mobilenetwork
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textview.MaterialTextView
+import java.util.*
 
 class LoginFragment : Fragment() {
 
@@ -16,6 +21,8 @@ class LoginFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
 
     // Views
+    private lateinit var emailInput: TextInputEditText
+    private lateinit var passwordInput: TextInputEditText
     private lateinit var loginBtn: MaterialButton
     private lateinit var googleBtn: MaterialButton
     private lateinit var facebookBtn: MaterialButton
@@ -33,21 +40,41 @@ class LoginFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
 
         // Bind views
+        emailInput = view.findViewById(R.id.login_fragment__email)
         loginBtn = view.findViewById(R.id.login_fragment__login_btn)
-        goToSignupBtn = view.findViewById(R.id.login_fragment__sign_up_btn)
+        googleBtn = view.findViewById(R.id.login_fragment__google_btn)
+        facebookBtn = view.findViewById(R.id.login_fragment__facebook_btn)
+        goToSignupBtn = view.findViewById(R.id.login_fragment__go_to_sign_up_btn)
 
         // Set listeners
-        loginBtn.setOnClickListener {
-
+        emailInput.addTextChangedListener {
+            if (Regex.EMAIL_REGEX.matches(emailInput.text.toString())) {
+                emailInput.error = null
+            } else {
+                emailInput.error = "Invalid Email"
+            }
         }
-
+        loginBtn.setOnClickListener {
+            Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
+        }
+        googleBtn.setOnClickListener {
+            googleBtn.setBackgroundColor(generateRandomColor())
+        }
+        facebookBtn.setOnClickListener {
+            facebookBtn.setBackgroundColor(generateRandomColor())
+        }
         goToSignupBtn.setOnClickListener {
             Log.i(LOG_TAG, "Go to Signup page btn was clicked")
-            viewModel.pageState.value = Pages.SIGNUP
+            viewModel.pageState.postValue(Pages.SIGNUP)
             Log.i(LOG_TAG, "New state is ${viewModel.pageState.value}")
         }
 
         return view
+    }
+
+    private fun generateRandomColor(): Int {
+        val rnd = Random()
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
     }
 
     companion object {
