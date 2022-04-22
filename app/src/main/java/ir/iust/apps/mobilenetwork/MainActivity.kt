07dec64b3576a372088a65d756e31a76bff9c3c4
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2022-04-23 00:30:20 +0430

    feat: add login fragment

--- app/src/main/java/ir/iust/apps/mobilenetwork/MainActivity.kt
+++ app/src/main/java/ir/iust/apps/mobilenetwork/MainActivity.kt
@@ -2,10 +2,26 @@ package ir.iust.apps.mobilenetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.iust.apps.mobilenetwork.utils.UiUtils
 
 class MainActivity : AppCompatActivity() {
+
+    private val loginFragment: LoginFragment = LoginFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        UiUtils().changeFragment(
+            supportFragmentManager,
+            loginFragment,
+            R.id.main_activity__main_frame,
+            true,
+            LOGIN_FRAGMENT_TAG
+        )
+    }
+
+    companion object {
+        const val LOGIN_FRAGMENT_TAG = "LOGIN_FRAGMENT"
     }
 }
